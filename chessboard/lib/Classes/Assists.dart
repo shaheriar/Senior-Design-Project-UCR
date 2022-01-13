000601e246b635c3d6f5731390ca8c45d2c80925
shaheriar <49822364+shaheriar@users.noreply.github.com>
2022-01-13 15:53:53 -0800

    difficulty settings

--- chessboard/lib/Classes/Assists.dart
+++ chessboard/lib/Classes/Assists.dart
@@ -7,7 +7,7 @@ class Assists {
   Map toJson() => {
         'player1': w, //RECOMMENDED MOVES FOR P1
         'player2': b, //RECOMMENDED MOVES FOR P2
-        'gamemode' : h, //VS HUMAN OR AI
+        'gamemode' : h, //VS HUMAN OR AI  1 = human, 2 = easy, 3 = medium, 4 = hard
         'move' : null,  //LAST MOVE DONE
         'usercolor' : check //COLOR OF THE USER
       };
