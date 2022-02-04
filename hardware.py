b47f7ae95d644d0c6d3771cedeffe54769940357
shaheriar <shaheriarm@gmail.com>
2022-02-03 17:58:32 -0800

    wrote stubs
    Co-Authored-By: Ryan Giron <36999776+rgiron1@users.noreply.github.com>
    Co-Authored-By: JShweiri <31330210+JShweiri@users.noreply.github.com>

--- hardware.py
+++ hardware.py
@@ -1,27 +1,62 @@
-from gpiozero import *
+# from gpiozero import *
 
-def chipSelect(pos):
-    # sends a binary value to be decoded into a single wire high. ie 011 => 00001000
-    pass
+class Hardware:
+    def getSquare():
+        # get what piece is on a square
+        pass
 
-def readData(pos):
-    chipSelect(pos)
-    # read data from the board
-    pass
+    def getBoard():
+        # get all the pieces on the board
+        pass
 
-def compareStates(prevBoard, currentBoard):
-    prevBoard = currentBoard
-    currentBoard = getBoard()
-    for i in range(0, 64):
-        if prevBoard[i] != currentBoard[i]:
-            return (i, prevBoard[i], currentBoard[i])
-    pass
+    def setLED(x,y,color):
+        # sets a square to a color
+        pass
 
-def getBoard():
-    board = []
-    for i in range(0, 64):
-        board.append(readData(i))
-    return board
+    def setLEDs(squares, legalMoves, color):    
+        # squares is a list of all the squares we want lit up
+        # legalMoves is a list of all the legal moves
+        # color is the color we want the squares to be
+        pass
+    
+    def compareBoard(prev, curr):
+        # returns nothing or a move
+        pass
 
-def lightLED(pos, color):
-    pass
+    def getMove():
+        # move = None
+        # while(move == None):
+        #    move = compareBoard(prev, curr)
+        # return move
+        pass
+
+"""
+Square = 'a2'
+allAvailableMoves = ['a2a4', …]
+
+//make comparison
+//light up corresponding squares
+For a in allavailablemoves:
+	If first two chars of a == square:
+		setLED(
+		Light up led of a's last two chars square
+
+getMove() :
+Move = nothing
+while(move) != nothing){
+Move = compareboard(prev, curr)
+}
+Return move
+
+// how to send piece picked up?
+
+Software side:
+Change to make:
+makeMove(self, board, depth, turn, historyFile):
+	Move = Hardware.getMove()
+
+convertAvailableMoves(listOfMoves):
+	listOfMoves -> string of possible moves
+"""
+
+    
